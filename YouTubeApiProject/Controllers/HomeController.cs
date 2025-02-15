dfa0edc40543f0e4bc170177f2ae11b2ef45951e
suhaila10 <117509232+suhaila10@users.noreply.github.com>
2025-02-16 03:51:34 +0800

    Update HomeController.cs

--- YouTubeApiProject/Controllers/HomeController.cs
+++ YouTubeApiProject/Controllers/HomeController.cs
@@ -1,32 +1,29 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using YouTubeApiProject.Models;
+using YouTubeApiProject.Services;
 
 namespace YouTubeApiProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly YouTubeApiService _youtubeService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(YouTubeApiService youtubeService)
         {
-            _logger = logger;
+            _youtubeService = youtubeService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
+            // Get Trending Videos and Music
+            var trendingVideos = await _youtubeService.GetTrendingVideosAsync();
+            var trendingMusic = await _youtubeService.GetTrendingMusicAsync();
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+            // Combine both into a single model
+            var model = new Tuple<List<YouTubeVideoModel>, List<YouTubeVideoModel>>(trendingVideos, trendingMusic);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // Pass the model to the view
+            return View(model);
         }
     }
 }
